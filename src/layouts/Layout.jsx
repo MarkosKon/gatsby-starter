47246c06f9ab7e09c660ef2a67b380698d5c050e
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-02-16 15:01:32 +0200

    Using the local Navbar in Layout #6.

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Navbar } from 'already-styled-components';
+import styled from 'styled-components';
+import { useTransition, animated } from 'react-spring';
 
+import { Navbar, DesktopList as BaseDL, MobileList } from '../components/Navbar';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 import { Box } from '../components/Primitives';
 import Link from '../components/Link';
+
+const DesktopList = styled(BaseDL)`
+  position: relative;
+`;
+const AnimatedMobileList = animated(MobileList);
 
 const Layout = ({ children }) => {
   const { title } = useSiteMetadata();
@@ -17,6 +24,25 @@ const Layout = ({ children }) => {
             <h1>{title}</h1>
           </Link>
 )}
+        desktopList={props => <DesktopList {...props} />}
+        mobileList={({ mobileMenuVisible, ...rest }) => {
+          const transitions = useTransition(mobileMenuVisible, null, {
+            from: { opacity: 0 },
+            enter: { opacity: 1 },
+            leave: { opacity: 0 },
+          });
+          return transitions.map(
+            ({ item, key, props }) => item && (
+            <AnimatedMobileList
+              {...rest}
+              key={key}
+              style={props}
+              alwaysVisible
+              mobileMenuVisible={mobileMenuVisible}
+            />
+            ),
+          );
+        }}
       >
         <Link to="/">Home</Link>
         <Link to="/page-2">Page 2</Link>
