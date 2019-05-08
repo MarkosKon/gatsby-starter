53d3e68c667b99c9faf8c7038978f8b60803e977
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-05-08 20:49:07 +0300

    Updated dependencies.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.(js|jsx)$/);
